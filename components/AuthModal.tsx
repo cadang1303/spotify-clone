6c7587798dc7d855fc895aa33f9d8e5fc1094cd7
cadang1303 <dangnguyen130399@gmail.com>
2023-07-27 09:36:56 +0700

    remove facebook login

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -40,7 +40,7 @@ const AuthModal = () => {
       <Auth
         theme="dark"
         magicLink
-        providers={["github", "google", "facebook"]}
+        providers={["github", "google"]}
         supabaseClient={supabaseClient}
         appearance={{
           theme: ThemeSupa,
